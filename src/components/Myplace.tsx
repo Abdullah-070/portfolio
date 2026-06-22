e07ee236bedd173c3c1149c924a04acf9ad243cd
Muhammad Abdullah <m.abdullahazhar070@gmail.com>
2026-06-22 18:01:23 +0500

    Add files via upload

--- src/components/Myplace.tsx
+++ src/components/Myplace.tsx
@@ -80,23 +80,6 @@ const Myplace = () => {
             <Marquee speed={30}>
               <div className="flex gap-5 items-center">
                 <div className="bg-gray-800 text-white py-2 px-4 rounded-lg">
-                  Jira
-                </div>
-                <Star className="text-primary-600" />
-                <div className="bg-gray-800 text-white py-2 px-4 rounded-lg">
-                  Clickup
-                </div>
-                <Star className="text-primary-600" />
-                <div className="bg-gray-800 text-white py-2 px-4 rounded-lg">
-                  Agile/Scrum Methodologies
-                </div>
-                <Star className="text-primary-600" />
-                <span></span>
-              </div>
-            </Marquee>
-            <Marquee direction="right" speed={30}>
-              <div className="flex gap-5 items-center">
-                <div className="bg-gray-800 text-white py-2 px-4 rounded-lg">
                   Rapid Prototyping
                 </div>
                 <Star className="text-primary-600" />
@@ -119,7 +102,7 @@ const Myplace = () => {
           </h3>
           <div className="relative w-full h-96 sm:h-[400px] lg:h-[400px] overflow-hidden rounded-lg">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3027.8474567890!2d74.294067!3d31.534645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391904deb8000001%3A0x1!2sSamanabad%2C%20Lahore%2C%20Pakistan!5e0!3m2!1sen!2spk!4v1733034398264!5m2!1sen!2spk"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27239.783468499456!2d74.20932539999998!3d31.4148718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3918fe2cfc4d8645%3A0x366027ad60688b9!2sLda%20Avenue%20Phase%201%20Lda%20Avenue%2C%20Lahore%2C%20Pakistan!5e0!3m2!1sen!2s!4v1782133056962!5m2!1sen!2s"
               className="absolute top-0 left-0 w-full h-full"
               loading="lazy"
               allowFullScreen
